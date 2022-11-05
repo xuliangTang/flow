9367097928cacfee11cc7f87a00206789f23ae9b
Lain <33924057+xuliangTang@users.noreply.github.com>
2022-11-05 22:47:04 +0800

    test fn (#1)
    * test fn
    
    * fix zero

--- src/main.go
+++ src/main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+func Div(v1 int, v2 int) int {
+	if v2 == 0 {
+		return 0
+	}
+	return v1 / v2
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
